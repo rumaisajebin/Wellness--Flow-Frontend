ba1411c4545ec0f1ad380d8df334f5b7f4e38871
rumaisajebin <jebinrumaisa@gmail.com>
2024-10-20 16:24:38 +0530

    websocket correcting afterhosting

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,7 +4,7 @@ export const BASE_URL = "http://rareblu.shop/";
 // export const BASE_URL = "http://127.0.0.1:8000/";
 
 export const PAYMENT_URL =
-  "http://127.0.0.1:8000/payment/payments/create_checkout_session/";
+  "http://rareblu.shop/payment/payments/create_checkout_session/";
 export const stripePromise = loadStripe(
   "pk_test_51PvfpaGFFuSDNKGDBkvHUBNNM8KISveSjN0cjPBFYX7OqoxJWqnw3bORe91cYA6qPF67whAs8lZVZsVHB0DuurAU00VkoKG4zC"
 ); 
