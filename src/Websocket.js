b926c097ac7b3b0a9a956cfce31093c85e4dab5c
rumaisajebin <jebinrumaisa@gmail.com>
2024-10-21 20:11:04 +0530

    websocket correcting afterhosting

--- src/Websocket.js
+++ src/Websocket.js
@@ -1,17 +1,17 @@
-const roomName = "room1"; // dynamically generate or retrieve room name
-const chatSocket = new WebSocket(
-    'wss://' + window.location.host + '/ws/chat/' + roomName + '/'
-);
+// const roomName = "room1"; // dynamically generate or retrieve room name
+// const chatSocket = new WebSocket(
+//     'wss://' + window.location.host + '/ws/chat/' + roomName + '/'
+// );
 
-chatSocket.onmessage = function(e) {
-    const data = JSON.parse(e.data);
-    console.log('Message received: ', data.message);
-};
+// chatSocket.onmessage = function(e) {
+//     const data = JSON.parse(e.data);
+//     console.log('Message received: ', data.message);
+// };
 
-chatSocket.onclose = function(e) {
-    console.error('Chat socket closed unexpectedly');
-};
+// chatSocket.onclose = function(e) {
+//     console.error('Chat socket closed unexpectedly');
+// };
 
-function sendMessage(message) {
-    chatSocket.send(JSON.stringify({ 'message': message }));
-}
+// function sendMessage(message) {
+//     chatSocket.send(JSON.stringify({ 'message': message }));
+// }
